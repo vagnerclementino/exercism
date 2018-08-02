2fd38ea0f68887b6b548d0b8aa048b58002a2833
unknown <pr003252@PC1510353.pbh.rmi>
2018-08-02 14:58:44 -0300

    Versão submetida ao mentor.

---
+++ java/matrix/src/main/java/RunMatrix.java
@@ -0,0 +1,14 @@
+public class RunMatrix{
+
+
+    public static void main(String[] args) {
+			String matrixAsString = "1";
+			int rowIndex = 0;
+			int[] expectedRow = {1};
+			
+			Matrix matrix = new Matrix(matrixAsString);
+			
+			
+        
+    }
+}
