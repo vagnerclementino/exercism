5b3b44a1a12c64d44478ad3d5b7f380bb7a71465
Vagner Clementino <vagner.clementino@gmail.com>
2020-07-11 19:17:42 -0300

    Adds package and comments to two_fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,12 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer allows to create a 2-fer message: One for you and one for me.
 package twofer
 
 import "fmt"
 
 const defaultName = "you"
 
-// ShareWith should have a comment documenting it.
+// ShareWith receives a name and returns the message "One for X, one for me".
+// The X is "you" when name is missing.
 func ShareWith(name string) string {
 	if name == "" {
 		name = defaultName
