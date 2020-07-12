73a0c5733c3ea2435ef6e26b47a597bd575ed464
Vagner Clementino <vagner.clementino@gmail.com>
2020-07-12 18:21:50 -0300

    Changes rna transcription exercice in elixir track based in mentor's tip

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -10,18 +10,15 @@ defmodule RnaTranscription do
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
     dna
-    |> List.to_string
-    |> String.codepoints
     |> Enum.map(fn(n) -> transpile(n) end)
   end
   
   def transpile(nucloide) do
     case nucloide do
-       "G" -> ?C
-       "C" -> ?G
-       "T" -> ?A
-       "A" -> ?U
+       ?G -> ?C
+       ?C -> ?G
+       ?T -> ?A
+       ?A -> ?U
     end
-    
   end
 end
