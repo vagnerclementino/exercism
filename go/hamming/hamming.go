884e3340885af0c9a15149cd6f4d8c43aba2dad6
Vagner Clementino dos Santos <vagner.clementino@gmail.com>
2020-07-23 10:55:41 -0300

    Change instations from distance variable to after validation

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -10,12 +10,11 @@ const errorDNASequenceNotEqual = "The DNA sequences must have equals length"
 // Distance calculates the hamming distance
 func Distance(a, b string) (int, error) {
 
-	var distance = 0
-
 	if len(a) != len(b) {
 		return 0, errors.New(errorDNASequenceNotEqual)
 	}
 
+	var distance = 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			distance++
