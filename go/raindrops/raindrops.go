539e74da83bff171fafc73e9b97ef1f27388e746
Vagner Clementino dos Santos <vagner.clementino@gmail.com>
2020-10-04 15:42:15 -0300

    Refactors raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,28 +2,23 @@ package raindrops
 
 import "fmt"
 
+var factors = [...]int{3, 5, 7}
+var results = map[int]string{
+	3: "Pling",
+	5: "Plang",
+	7: "Plong",
+}
+
 // Convert a number into a string that contains raindrop sounds corresponding
 // to certain potential factors
 func Convert(input int) string {
 	var result string
-	var isFactor bool
-
-	if input%3 == 0 {
-		result = "Pling"
-		isFactor = true
+	for _, factor := range factors {
+		if input%factor == 0 {
+			result = result + results[factor]
+		}
 	}
-
-	if input%5 == 0 {
-		result = result + "Plang"
-		isFactor = true
-	}
-
-	if input%7 == 0 {
-		result = result + "Plong"
-		isFactor = true
-	}
-
-	if !isFactor {
+	if result == "" {
 		result = fmt.Sprintf("%d", input)
 	}
 	return result
