18e8fa011c3ad376d8c6743f87d37b51be9a098e
Vagner Clementino dos Santos <vagner.clementino@gmail.com>
2020-11-03 19:45:46 -0300

    Refactors ResistorColor class

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,33 +1,39 @@
 export class ResistorColor {
   private colors: string[];
-  private values: Map<string, string>;
+  private resistorsValues: Map<string, string>;
 
   constructor(colors: string[]) {
-    if (colors.length >=2){
+    if (this.isValid(colors)) {
       this.colors = colors;
-      this.values = new Map<string, string>();
-      this.values.set("black", "0")
-      this.values.set("brown", "1")
-      this.values.set("orange", "3")
-      this.values.set("yellow", "4")
-      this.values.set("green", "5")
-      this.values.set("blue", "6")
-      this.values.set("violet", "7")
-      this.values.set("grey", "8")
+      this.resistorsValues = this.loadResistorValues();
     } else {
-      throw new Error("At least two colors need to be present")
+      throw new Error("At least two colors need to be present");
     }
   }
-  
+
   value = (): number => {
-    let result = ""
-    for( const color of this.colors){
-      const value  = this.values.get(color)
-      result += value
+    let result = "";
+    for (const color of this.colors) {
+      result += this.resistorsValues.get(color);
       if (result.length == 2) {
-        break
+        break;
       }
     }
     return Number(result);
-  }
+  };
+
+  private isValid = (colors: string[]): boolean => colors.length >= 2;
+
+  private loadResistorValues = (): Map<string, string> => {
+    const resitorsValues = new Map<string, string>();
+    resitorsValues.set("black", "0");
+    resitorsValues.set("brown", "1");
+    resitorsValues.set("orange", "3");
+    resitorsValues.set("yellow", "4");
+    resitorsValues.set("green", "5");
+    resitorsValues.set("blue", "6");
+    resitorsValues.set("violet", "7");
+    resitorsValues.set("grey", "8");
+    return resitorsValues;
+  };
 }
