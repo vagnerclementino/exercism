ceeefd70e8fcda143d625a26ffc142c12d86e189
Vagner Clementino dos Santos <vagner.clementino@gmail.com>
2020-11-04 14:48:46 -0300

    Adding first version from count method

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,24 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    String.replace(sentence, "_", " ")
+    |> String.replace(",", "")
+    |> String.replace(":", "")
+    |> String.replace("!", "")
+    |> String.replace("&", "")
+    |> String.replace("@", "")
+    |> String.replace("$", "")
+    |> String.replace("%", "")
+    |> String.replace("^", "")
+    |> String.downcase()
+    |> String.split(" ", trim: true)
+    |> Enum.reduce(%{}, fn word, result ->
+      Map.update(result, word, 1, fn counter -> counter + 1 end)
+    end)
+  end
+
+  def normalize(word) do
+    word
+    |> String.replace(word, ",", "")
   end
 end
