313b9ea2cf4c3e24b2e216ff93ff96ca369a7444
Vagner Clementino do Santos <vagner.clementino@HOTBHZA004390.hotmart.local>
2020-12-15 21:20:13 -0300

    [lua]Refactors compute method

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,15 +1,15 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
-    if #a ~= #b then
-        return -1
-    end
-
     local diff = 0
-    for i=1, #a do
-       if string.sub(a, i,i) ~= string.sub(b,i,i) then
-           diff = diff + 1
-       end
+    if #a ~= #b then
+        diff = -1
+    else
+        for i=1, #a do
+           if string.sub(a, i,i) ~= string.sub(b,i,i) then
+               diff = diff + 1
+           end
+        end
     end
     return diff
 end
